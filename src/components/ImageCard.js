660a07b984ec5da7b110700114d281a9ed942738
quartzes <quartzes@kaist.ac.kr>
2021-05-13 17:29:03 +0900

    파일 추가: ImageCard.js (이미지 카드) 파일 추가: fish.jpg (Default Image) 파일 수정: Home.js (이미지 카드 배치)

---
+++ src/components/ImageCard.js
@@ -0,0 +1,46 @@
+import React from "react"
+import styled from "styled-components"
+
+const ImageCard = ({ src, title, children }) => {
+    return (
+        <CardContainer>
+            <ImageSlot src = {src}/>
+            <CardHeader>
+                <Title>
+                    {title}
+                </Title>
+            </CardHeader>
+            <CardBody>
+                {children}
+            </CardBody>
+        </CardContainer>
+    )
+}
+
+const CardContainer = styled.div`
+    height: 100%;
+    border-radius: 5px;
+    border: 1px solid #f0f0f0;
+    padding: 10px 5px 10px 5px;
+`;
+
+const ImageSlot = styled.img`
+    width: 550px;
+`;
+
+const CardHeader = styled.div`
+    padding-bottom: 5px;
+    border-bottom: 1px solid #f0f0f0;
+    color: rgba(0,0,0,.85);
+`;
+
+const CardBody = styled.div`
+    margin-top: 10px;
+`;
+
+const Title = styled.div`
+    font-weight: bold;
+`;
+
+
+export default ImageCard;
