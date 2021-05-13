1fda16ca4fb9c5a05e86586e201977084c7c3159
nahjh2 <nahjh2@gmail.com>
2021-05-14 06:04:58 +0900

    for axios test

--- src/components/Header.ctrl.js
+++ src/components/Header.ctrl.js
@@ -2,38 +2,39 @@
 
 import DefaultImage from "../images/fish.jpg";
 import { apiURL } from "../settings";
+import axios from "axios";
 
-// 임시
-const GetFishes = () => {
-  var fishes = [
-    {
-      fishCode: 0,
-      fishName: "참돔",
-      description: "제철시기: 3~5월",
-      imageURL: DefaultImage,
-    },
-    {
-      fishCode: 1,
-      fishName: "숭어",
-      description: "제철시기: 3~5월",
-      imageURL: DefaultImage,
-    },
-    {
-      fishCode: 2,
-      fishName: "볼락",
-      description: "제철시기: 3~5월",
-      imageURL: DefaultImage,
-    },
-  ];
+// // 임시
+// const GetFishes = () => {
+//   var fishes = [
+//     {
+//       fishCode: 0,
+//       fishName: "참돔",
+//       description: "제철시기: 3~5월",
+//       imageURL: DefaultImage,
+//     },
+//     {
+//       fishCode: 1,
+//       fishName: "숭어",
+//       description: "제철시기: 3~5월",
+//       imageURL: DefaultImage,
+//     },
+//     {
+//       fishCode: 2,
+//       fishName: "볼락",
+//       description: "제철시기: 3~5월",
+//       imageURL: DefaultImage,
+//     },
+//   ];
 
-  console.log("getfish");
+//   console.log("getfish");
 
-  return fishes;
-};
-
-// const GetFishes = async () => {
-//   const response = await axios.get(`${apiURL}/`);
-//   return response.data;
+//   return fishes;
 // };
+
+const GetFishes = async () => {
+  const response = await axios.get(`${apiURL}/fishes`);
+  return response.data;
+};
 
 export { GetFishes };
