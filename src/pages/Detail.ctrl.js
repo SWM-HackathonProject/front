d0de4bcc611bb7a1d6df11bc53f2bbb25ef296e8
nahjh2 <nahjh2@gmail.com>
2021-05-14 07:28:35 +0900

    ..

--- src/pages/Detail.ctrl.js
+++ src/pages/Detail.ctrl.js
@@ -5,7 +5,7 @@ import { apiURL } from "../settings";
 import axios from "axios";
 
 const GetFishInfo = async ({ fishCode }) => {
-  const response = await axios.get(`${apiURL}/fishImg/${fishCode}.jpeg`);
+  const response = await axios.get(`${apiURL}/fishes/${fishCode}`);
   return response.data;
 };
 
