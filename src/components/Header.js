165a0b78852c678cf046c5117cec47ef6cac31cc
nahjh2 <nahjh2@gmail.com>
2021-05-13 22:41:26 +0900

    dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,106 +1,145 @@
+/** @format */
+
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { Input } from "antd";
 
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBDropdownLink,
+} from "mdb-react-ui-kit";
+
 const Header = () => {
-    return (
-        <Container>
-            <LogoArea>
-                <Logo src="https://swmaestro.org/static/sw/images/logo_w.png" />
-            </LogoArea>
-            <SearchBar placeholder="어종을 검색하세요." />
-            <LinkArea>
-                <LinkText to='/'>홈</LinkText>
-            </LinkArea>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <LogoArea>
+        <Logo src='https://swmaestro.org/static/sw/images/logo_w.png' />
+      </LogoArea>
+      <SearchBar placeholder='어종을 검색하세요.' />
+
+      <MDBDropdown>
+        <LinkArea>
+          <MDBDropdownToggle tag='a' className='nav-link'>
+            <LinkText to='/'>홈</LinkText>
+          </MDBDropdownToggle>
+        </LinkArea>
+        <MDBDropdownMenu>
+          <MDBDropdownItem>
+            <MDBDropdownLink href='/about'>About</MDBDropdownLink>
+          </MDBDropdownItem>
+          <MDBDropdownItem>
+            <MDBDropdownLink href='https://github.com/SWM-HackathonProject'>
+              Git hub
+            </MDBDropdownLink>
+          </MDBDropdownItem>
+        </MDBDropdownMenu>
+      </MDBDropdown>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    width: 100%;
-    height: 90px;
-    background-color: skyblue;
-    align-content: space-around;
-    display: flex;
-    justify-content: center;
-    position: relative;
-    background-image: url("https://i.pinimg.com/736x/da/90/2d/da902defe491c20a8ebde6c7207276fd.jpg");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    
-    ::before {
-        content: "";
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        height: 10px;
-        border-bottom: 5px solid white;
-        background-size: 20px 40px;
-        background-image:
-            radial-gradient(circle at 10px -15px, transparent 20px, white 21px);
-    }
+  width: 100%;
+  height: 90px;
+  background-color: skyblue;
+  align-content: space-around;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  background-image: url("https://i.pinimg.com/736x/da/90/2d/da902defe491c20a8ebde6c7207276fd.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 
-    ::after {
-        content: "";
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        height: 15px;
-        border-bottom: 5px solid white;
-        background-size: 40px 40px;
-        background-image:
-            radial-gradient(circle at 10px 26px, white 20px, transparent 21px);
-    }
-`
+  ::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 10px;
+    border-bottom: 5px solid white;
+    background-size: 20px 40px;
+    background-image: radial-gradient(
+      circle at 10px -15px,
+      transparent 20px,
+      white 21px
+    );
+  }
+
+  ::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 15px;
+    border-bottom: 5px solid white;
+    background-size: 40px 40px;
+    background-image: radial-gradient(
+      circle at 10px 26px,
+      white 20px,
+      transparent 21px
+    );
+  }
+`;
 
 const LogoArea = styled.div`
-    padding: 18px 50px 0px 18px;
+  padding: 18px 50px 0px 18px;
 
-    @media only screen and (max-width: 1000px) {
-        display: none;
-    }
-`
+  @media only screen and (max-width: 1000px) {
+    display: none;
+  }
+`;
 
-const Logo = styled.img`
-`
+const Logo = styled.img``;
 
 const SearchBar = styled(Input)`
-    width: 100%;
-    height: 40px;
-    border-radius: 5px;
-    border-style: solid;
-    font-size: 20px;
-    padding: 0px 10px 0px 10px;
-    margin-top: 17px;
+  width: 100%;
+  height: 40px;
+  border-radius: 5px;
+  border-style: solid;
+  font-size: 20px;
+  padding: 0px 10px 0px 10px;
+  margin-top: 17px;
 
-    :focus {
-        outline: none;
-    }
+  :focus {
+    outline: none;
+  }
 
-    @media only screen and (max-width: 1000px) {
-        width: 80%;
-    }
-`
+  @media only screen and (max-width: 1000px) {
+    width: 80%;
+  }
+`;
 
 const LinkArea = styled.div`
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    margin-top: 25px;
-    margin: 25px 100px 0px 100px;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  margin-top: 25px;
+  margin: 25px 100px 0px 100px;
 
-    @media only screen and (max-width: 1000px) {
-        display: none;
-    }
-`
+  @media only screen and (max-width: 1000px) {
+    display: none;
+  }
+`;
 
 const LinkText = styled(Link)`
-    color: white;
-    font-weight: bold;
-`
+  color: white;
+  font-weight: bold;
+`;
 
 export default Header;
